77ba5c501fcdea3153ff4c9acaea2ecfbc17aaa1
AbdulazizSulaymon <djshahrick@gmail.com>
2022-04-15 16:48:26 +0500

    loading

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,15 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router, { useRouter } from "next/router";
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
